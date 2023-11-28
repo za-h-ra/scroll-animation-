61a5708cd81c1a809e2cf70cb6766ede72b4f285
za-h-ra <zahra.khan1@icloud.com>
2023-11-28 17:37:04 -0500

    add API integration and animation

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-const boxes = document.querySelectorAll(".box");
+// API
+const url = "https://stars-api.p.rapidapi.com/starslist";
 
-window.addEventListener("scroll", checkBoxes);
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "66f73b6c79mshfc9febeb400ab27p115c2djsn13f5c3cd76f1",
+    "X-RapidAPI-Host": "stars-api.p.rapidapi.com",
+  },
+};
 
-function checkBoxes() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
+async function fetchData() {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data);
 
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
+    const starsListElement = document.getElementById("stars-list");
 
-    if (boxTop < triggerBottom) {
-      box.classList.add("show");
-    } else {
-      box.classList.remove("show");
-    }
-  });
+    data.forEach((star) => {
+      console.log(`STAR ${star.name}`);
+      const starElement = document.createElement("div");
+      starElement.classList.add("star-element");
+      starElement.innerHTML = `Star Name: <br> ${star.name}`;
+      starsListElement.appendChild(starElement);
+
+      // ANIMATION
+
+      const starsList = document.querySelectorAll(".star-element");
+      window.addEventListener("scroll", checkBoxes);
+      function checkBoxes() {
+        const triggerBottom = (window.innerHeight / 5) * 4;
+        starsList.forEach((stars) => {
+          const starTop = stars.getBoundingClientRect().top;
+
+          if (starTop < triggerBottom) {
+            stars.classList.add("show");
+          } else {
+            stars.classList.remove("show");
+          }
+        });
+      }
+
+      checkBoxes();
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-checkBoxes();
+fetchData();
